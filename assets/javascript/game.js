97ace056aa0d984a34f7463a6d8ad5ba734d23fb
elwdipath <jordan.kaiser@gmail.com>
2019-12-27 10:52:36 -0500

    pseudo-coded pyschic game and created variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// Create var for wins/losses/chances/and guesses so far
+    var wins = 0;
+    var losses = 0;
+    var chances = 10;
+    var guesses = 0; 
+
+// computer picks random letter
+
+// program logs user keystroke and compares to random letter computer picked
+
+// if user guesses correctly, add 1 to the wins variable and 1 to the guesses so far var
+
+// if user guesses incorrectly, add 1 to the losses var and subtract 1 from the guesses left var. 
