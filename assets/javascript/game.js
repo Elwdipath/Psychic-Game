d9ed049abe445ab56779acd182608342c8077339
elwdipath <jordan.kaiser@gmail.com>
2019-12-29 10:47:56 -0500

    fixed completed functions and conditionals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,61 @@
 // Create var for wins/losses/chances/and guesses so far
-    var wins = 0;
-    var losses = 0;
-    var chances = 10;
-    var guesses = 0; 
-    var letter;
+var psychic = {
+    wins: 0,
+    losses: 0,
+    chances: 10,
+    guesses: [],
+    computerLetter: [],
+    letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+}
 
 // computer picks random letter
 function randLetter() {
-    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    letter = letters[Math.floor(Math.random() * letters.length)];
-    return letter
+    psychic.computerLetter = psychic.letters[Math.floor(Math.random() * psychic.letters.length)];
+    return psychic.computerLetter;
 }
 
-// program logs user keystroke and compares to random letter computer picked
+function start() {
+    document.getElementById("wins").innerText = 'Wins: ${psychic.wins}' ;
+    document.getElementById("losses").innerText = 'Losses: ${psychic.losses}' ;
+    document.getElementById("chancesLeft").innerText = 'Chances: ${psychic.chances}' ;
+    document.getElementById("guessesMade").innerText = 'Guesses: ${psychic.guesses' ;
+    
+}
+
+function gameWin() {
+    psychic.wins++ ;
+    psychic.chances = 10 ;
+    document.getElementById("wins").innerText = 'Wins: ${psychic.wins}' ;
+    document.getElementById("chancesLeft").innerText = 'Chances Left: ${psychic.chances}' ;
+    document.getElementById("guessesMade").innerText = 'Your Guesses: ${psychic.guesses}' ;
+}
+
+function gameLose() {
+    psychic.losses++ ;
+    psychic.chances-- ;
+    document.getElementById("losses").innerText = 'Losses: ${psychic.losses}' ;
+    document.getElementById("chancesLeft").innerText = 'Chances Left: ${psychic.chances}' ;
+    document.getElementById("guessesMade").innerText = 'Your Guesses: ${psychic.guesses}' ;
+}
+
+// computer selects random letter
+computerGuess = randLetter()
 
 document.onkeyup = function(event) {
     var userKey = event.key.toLowerCase();
+    
 
-}
 // if user guesses correctly, add 1 to the wins variable and 1 to the guesses so far var
 
-// if user guesses incorrectly, add 1 to the losses var and subtract 1 from the guesses left var. 
+    if (computerGuess === userKey) {
+        alert("Correct!")
+        gameWin() ;
+    }
+    // if user guesses incorrectly, add 1 to the losses var and subtract 1 from the guesses left var.
+    else {
+        alert("Nope. Try Again.")
+        gameLose() ;
+    }
+ 
+
+}
