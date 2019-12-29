b462c8bdbbb0391e240b0036b07a240cf637b98b
elwdipath <jordan.kaiser@gmail.com>
2019-12-29 12:07:32 -0500

    added condition if user selected wrong key and if user selected key already guessed.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,50 +4,70 @@ var psychic = {
     losses: 0,
     chances: 10,
     guesses: [],
-    computerLetter: [],
+    computerLetter: "",
     letters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
 }
 
-// computer picks random letter
+// random letter function
 function randLetter() {
     psychic.computerLetter = psychic.letters[Math.floor(Math.random() * psychic.letters.length)];
-    return psychic.computerLetter;
 }
-
+// game start
 function start() {
-    document.getElementById("wins").innerText = 'Wins: ${psychic.wins}' ;
-    document.getElementById("losses").innerText = 'Losses: ${psychic.losses}' ;
-    document.getElementById("chancesLeft").innerText = 'Chances: ${psychic.chances}' ;
-    document.getElementById("guessesMade").innerText = 'Guesses: ${psychic.guesses' ;
+    psychic.chances = 10 ;
+    psychic.guesses = [] ;
+    document.getElementById("wins").innerText = `Wins: ${psychic.wins}` ;
+    document.getElementById("losses").innerText = `Losses: ${psychic.losses}` ;
+    document.getElementById("chancesLeft").innerText = `Chances: ${psychic.chances}` ;
+    document.getElementById("guessesMade").innerText = `Your Guesses: ${psychic.guesses.join(" ").toUpperCase()}` ;
+    randLetter();
+    
+}
+// game win function
+function gameWin() {
+    psychic.wins++ ;
+    start()
+}
+// game lose function
+function gameLose() {
+    psychic.chances-- ;
+    if (psychic.chances < 1){
+        alert("Game Over")
+        psychic.losses++
+        start()
+        return
+    }
+    document.getElementById("chancesLeft").innerText = `Chances Left: ${psychic.chances}`;
+    document.getElementById("guessesMade").innerText = `Your Guesses: ${psychic.guesses.join(" ")}` ;
     
 }
 
-function gameWin() {
-    psychic.wins++ ;
-    psychic.chances = 10 ;
-    document.getElementById("wins").innerText = 'Wins: ${psychic.wins}' ;
-    document.getElementById("chancesLeft").innerText = 'Chances Left: ${psychic.chances}' ;
-    document.getElementById("guessesMade").innerText = 'Your Guesses: ${psychic.guesses}' ;
-}
+// computer selects random letter by calling randLetter function
 
-function gameLose() {
-    psychic.losses++ ;
-    psychic.chances-- ;
-    document.getElementById("losses").innerText = 'Losses: ${psychic.losses}' ;
-    document.getElementById("chancesLeft").innerText = 'Chances Left: ${psychic.chances}' ;
-    document.getElementById("guessesMade").innerText = 'Your Guesses: ${psychic.guesses}' ;
-}
-
-// computer selects random letter
-computerGuess = randLetter()
+start()
 
 document.onkeyup = function(event) {
     var userKey = event.key.toLowerCase();
-    
 
+// check if character input is a letter and alert if it is invalid.
+
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+       //validate user hasn't already guessed that key. 
+        if (psychic.guesses.includes(userKey)) {
+        alert("Already guessed");
+        return; 
+       }
+        psychic.guesses.push(userKey);
+    }
+    
+    else {
+        alert("Invalid character")
+        return; 
+    }
+    
 // if user guesses correctly, add 1 to the wins variable and 1 to the guesses so far var
 
-    if (computerGuess === userKey) {
+    if (psychic.computerLetter === userKey) {
         alert("Correct!")
         gameWin() ;
     }
@@ -55,7 +75,9 @@ document.onkeyup = function(event) {
     else {
         alert("Nope. Try Again.")
         gameLose() ;
+
     }
  
+    
 
 }
